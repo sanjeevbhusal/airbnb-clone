d6b127f094452fa7695a46bb1f366f980e56c417
sanjeev bhusal <bhusalsanjeev23@gmail.com>
2023-09-16 14:27:16 +0545

    feat: close menu item if clicked on the DOM

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,11 +2,33 @@
 
 import { BiMenu } from "react-icons/bi";
 import Image from "next/image";
-import { useState } from "react";
+import { createRef, useEffect, useState } from "react";
 import { MenuItem } from "./MenuItem";
 
 export function UserMenu() {
   const [openMenu, setOpenMenu] = useState(false);
+  const menuItemRef = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    // If Menu Item is not in DOM, there is no need to add a Event listener.
+    if (!menuItemRef.current) {
+      return;
+    }
+
+    const eventHandler = () => {
+      if (!openMenu) {
+        return;
+      }
+
+      setOpenMenu(false);
+    };
+
+    document.addEventListener("click", eventHandler);
+
+    return () => {
+      document.removeEventListener("click", eventHandler);
+    };
+  }, [openMenu]);
 
   return (
     <div className="flex-row gap-4 items-center hidden md:flex relative">
@@ -29,7 +51,10 @@ export function UserMenu() {
         />
       </div>
       {openMenu ? (
-        <div className="absolute right-0 top-[120%] w-48 border rounded-md shadow bg-white">
+        <div
+          className="absolute right-0 top-[120%] w-48 border rounded-md shadow bg-white"
+          ref={menuItemRef}
+        >
           <MenuItem label="Sign up" isBold onClick={() => {}} />
           <MenuItem label="Log in" onClick={() => {}} />
           <MenuItem label="Help center" onClick={() => {}} />
